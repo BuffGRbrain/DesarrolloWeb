94d5f15373bcc38d111ca810a4d5a5e8c5b3a4e0
Guillermo-Ribero <62837537+BuffGRbrain@users.noreply.github.com>
2023-11-01 12:35:38 -0500

    intro js

---
+++ PracticaHTML/assets/js/intro.js
@@ -0,0 +1,16 @@
+//Es isomorfico pq se puede ejecutar en las 3 capas de aplicación web. Front, Back y Base de datos que es la de persistencia.
+
+//Js para correr en bakc necesita un motor como nodejs que permite usar express
+//se pueden declara variables con nombres como $nombre, _nombre, nombre. Usa camelCase.
+
+//console.log("como un print") Muestra el output en consola del navegador. 
+
+//usar back comillas `` o no funciona la inclusión de variables.
+
+let a = 3;
+let b = 2;
+
+console.log(`El resultado de la suma de ${a} y ${b} es:  ${a+b}`)
+
+
+
